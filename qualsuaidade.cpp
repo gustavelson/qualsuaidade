bf4796a879f34d31c1fe6b32576a13bf0733f815
Gustavo Souza <78645956+gustavelson@users.noreply.github.com>
2021-11-30 11:10:32 -0300

    Add files via upload

---
+++ qualsuaidade.cpp
@@ -0,0 +1,14 @@
+#include
+using namespace std;
+int main()
+{
+	int anoAtua, anoNasc,idade;
+	count<<"Ano atual: ";
+	cin>> anoAtual;
+	count<<"Ano de nascimento:";
+	cin>> anoNasc;
+	idade=anoAtual-anoNasc;
+	count<<"Você tem ou fará " << idade << " anos.";
+}
+
+
